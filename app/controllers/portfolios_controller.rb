e525e643211f877d4abf7f4fc24555ea639acc95
JJ Keeney <jjkeeney129285@gmail.com>
2017-08-25 15:34:15 -0600

    Implemented portfolio CRUD functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,26 @@
 class PortfoliosController < ApplicationController
 
-def index
-@port = Portfolio.all
-end
+  def index
+    @port = Portfolio.all
+  end
 
+  def new
+    @port = Portfolio.new
+  end
 
+  def create
+    @port = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, :thumb_image ))
+
+    respond_to do |format|
+      if @port.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item exists now!!' }
+        
+      else
+        format.html { render :new }
+       
+      end
+    end
+  end
 
 
 
