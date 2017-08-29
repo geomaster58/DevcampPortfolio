b15bd3acb5e73cc0cd55f475bb7788a7cdfde79c
JJ Keeney <jjkeeney129285@gmail.com>
2017-08-29 15:02:41 -0600

    added concerns and defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,7 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
+
   validates_presence_of :title, :body, :main_image, :thumb_image
 
 
@@ -20,9 +23,8 @@ class Portfolio < ApplicationRecord
 
   def set_defaults
     
-    self.main_image ||= "http://via.placeholder.com/600x400"
-    self.thumb_image ||= "http://via.placeholder.com/350x200"
-
+    self.main_image ||= Placeholder.img(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.img(height: '350', width: '200')
   end
 
 end
