18f2407eb7b505393e003b229fb72efd32750900
JJ Keeney <jjkeeney129285@gmail.com>
2017-09-03 13:53:52 -0600

    implemented content helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,18 @@ module ApplicationHelper
     link_to "Logout", destroy_user_session_path, method: :delete 
     else 
      (link_to "Register", new_user_registration_path, method: :get ) +
-     "<br>".html_safe +    
+
+     "<br>".html_safe + 
+
      (link_to "Sign In", new_user_session_path, method: :get) 
     end 
+  end
+  
+  def source_helper(layout_name)
+    if session[:source]
+      content_tag(:p, "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout", class: "source-greeting") 
 
 
+    end
   end
 end
