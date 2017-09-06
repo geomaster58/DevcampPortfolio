b7dc1f31c176daa8fc07bb807639214b48582ed4
JJ Keeney <jjkeeney129285@gmail.com>
2017-09-06 12:22:10 -0600

    implemented all the authorizations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
  def login_helper
-    if current_user.is_a?(User) 
-    link_to "Logout", destroy_user_session_path, method: :delete 
-    else 
-     (link_to "Register", new_user_registration_path, method: :get ) +
+    if current_user.is_a?(GuestUser)
+    (link_to "Register", new_user_registration_path, method: :get ) +
 
      "<br>".html_safe + 
 
-     (link_to "Sign In", new_user_session_path, method: :get) 
+     (link_to "Sign In", new_user_session_path, method: :get)  
+     
+    else 
+     link_to "Logout", destroy_user_session_path, method: :delete
     end 
   end
   
