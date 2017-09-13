7dd0d26cac84aa62c8281b06b2ae0e8f9a0888cf
JJ Keeney <jjkeeney129285@gmail.com>
2017-09-13 11:22:38 -0600

    added technology adding for portfolio and put in an edit item button on the show page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -25,7 +25,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @ports = Portfolio.new
-    3.times { @ports.technologies.build }
+    @ports.technologies.build 
   end
 
   def show
@@ -81,7 +81,7 @@ class PortfoliosController < ApplicationController
                                       :body, 
                                       :thumb_image, 
                                       :main_image, 
-                                      technologies_attributes: [:name])                                      
+                                      technologies_attributes: [:id, :name, :_destroy])                                      
   end
 
   def set_portfolio_item
